0b3e7b626678c63bb7326152bdc72770d1100ef6
HsiaoShan <orz322@gmail.com>
2018-03-08 19:03:41 +0800

    Create project

---
+++ SwiftGirls201804/SwiftGirls201804/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftGirls201804
+//
+//  Created by HsiaoShan on 2018/3/8.
+//  Copyright © 2018年 SwiftGirls. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
