95e76bcffff67b0c8040d2ff09bec1f875dbc27d
candyhu079 <candyhu079@hotmail.com>
2018-04-14 22:40:24 +0800

    多加一些註解，希望你們看得懂

--- SwiftGirls201804/SwiftGirls201804/DataApi.swift
+++ SwiftGirls201804/SwiftGirls201804/DataApi.swift
@@ -9,17 +9,16 @@
 import UIKit
 
 private var serverUrl: String = "http://opendata.cwb.gov.tw/govdownload?dataid=E-A0015-001R&authorizationkey=rdec-key-123-45678-011121314"
-private var cookie = "TS01dbf791=0107dddfef75ce76139a469c1d2d19b9ae4760afeee06056375e5fa7c162241c034c7fb0f0"
 private var taiwanCountyArray = ["基隆市", "臺北市", "新北市", "桃園市", "新竹市", "新竹縣", "苗栗縣", "臺中市", "南投縣", "彰化縣", "雲林縣", "嘉義市", "嘉義縣", "臺南市", "高雄市", "屏東縣", "宜蘭縣", "花蓮縣", "臺東縣", "澎湖縣", "金門縣", "連江縣"]
 
 class DataApi: NSObject, XMLParserDelegate {
     static var shared: DataApi = DataApi()
-    private static var earthquake: Earthquake? = nil
+    private static var earthquake: Earthquake? = nil  //用這個裝Api 取回的資料
 
-    private var elementName: String = ""
-    private var shakingArea: ShakingArea? = nil
-    private var stationsArray: [EqStation] = []
-    private var eqStation: EqStation? = nil
+    private var elementName: String = ""  //記錄XMLParser 目前取到的資料名稱
+    private var shakingArea: ShakingArea? = nil  //記錄XMLParser 取到的shakingArea(縣市)
+    private var stationsArray: [EqStation] = []  //記錄地震測站的陣列
+    private var eqStation: EqStation? = nil  //記錄XMLParser 取到的eqStation(測站)
     
     class func getData(completion: @escaping (_ data: Earthquake?, _ error: Error?) -> Void ) {
         let url = URL(string: serverUrl)
@@ -27,26 +26,25 @@ class DataApi: NSObject, XMLParserDelegate {
             return
         }
         
-        var urlRequest = URLRequest(url: url!)
-        urlRequest.addValue(cookie, forHTTPHeaderField: "Cookie")
-        
-        let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+        let task = URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
             guard error == nil && data != nil else {
                 return
             }
             let xmlParser = XMLParser(data: data!)
             xmlParser.delegate = self.shared
             
-            let success = xmlParser.parse()
+            let success = xmlParser.parse()  //success 是個Boolean 值，紀錄xmlParser 讀取XML 成功與否
             if success {
+                // 如果讀取XML 成功，把抓到的earthquake 物件丟給呼叫這個func 的人，且跟他說error 是nil
                 completion(earthquake, nil)
             } else {
+                // 讀取XML 失敗，產生一個Error 的物件，丟給呼叫這個func 的人，且跟他說data 是nil
                 completion(
                     nil,
                     NSError(domain:"Error", code:999, userInfo:["message":"資料錯誤"])
                 )
             }
-        }
+        })
         task.resume()
     }
     
@@ -74,10 +72,7 @@ class DataApi: NSObject, XMLParserDelegate {
         } else if elementName == "eqStation" {
             eqStation = EqStation()
         }
-        guard (string.trimmingCharacters(in: [" ", "\n"])).count > 0 else {
-            return
-        }
-        
+ 
         //earthquake
         if elementName == "earthquakeNo" {
             DataApi.earthquake?.earthquakeNo += string
@@ -138,16 +133,19 @@ class DataApi: NSObject, XMLParserDelegate {
     
     func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
         // parse 到element結束的tag
-        if elementName == "shakingArea" && shakingArea != nil {
+        // 從資料來看，earthquake 裡面的intensity 裡面的shakingArea(縣市)包含了很多eqStation(測站)，所以需要在每一個eqStation 物件做好的時候，把eqStation 加進stationsArray 這個陣列，這個陣列是我在類別最上方宣告用來裝eqStation 的陣列
+        if elementName == "eqStation" && eqStation != nil {
+            stationsArray.append(eqStation!)
+        }
+        // 從資料來看，earthquake 裡面的intensity 包含了很多shakingArea(縣市)，所以需要在每一個shakingArea 物件做好的時候，先把做滿eqStation 的stationsArray 放進自己底下(shakingArea 裡面有一個屬性是stationsArray)，再把shakingArea 加進earthquake 裡面的intensityArray 這個陣列
+        else if elementName == "shakingArea" && shakingArea != nil {
             shakingArea!.stationsArray = stationsArray
             DataApi.earthquake?.intensityArray?.append(shakingArea!)
-        } else if elementName == "eqStation" && eqStation != nil {
-            stationsArray.append(eqStation!)
         }
     }
     
     func parserDidEndDocument(_ parser: XMLParser) {
-        // 整份文件parse 完畢時進來
+        // 整份文件parse 完畢時進來，把資料裡面shakingArea 不是在敘述縣市的資料過濾掉
         let shakingAreaArray = DataApi.earthquake?.intensityArray?.filter({ (shakingArea) -> Bool in
             taiwanCountyArray.contains(String(shakingArea.areaDesc.prefix(3)))
         })
